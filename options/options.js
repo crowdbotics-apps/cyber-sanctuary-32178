c36b57698f8348d161862bf6eec0f570f39af032
Crowdbotics <team@crowdbotics.com>
2021-11-27 14:38:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cyber_sanctuary_32178",
+  url: "https://cyber_sanctuary_32178.botics.co",
+  api: "https://cyber_sanctuary_32178.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
